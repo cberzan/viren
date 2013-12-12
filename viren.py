c568ba436f3729d4f9020937be6b229f19a9adaa
Constantin Berzan <cberzan@gmail.com>
2013-12-12 15:27:29 -0800

    refactor and add tests

--- viren.py
+++ viren.py
@@ -34,30 +34,73 @@ def verify_rename_list(old_names, new_names):
         seen_names.add(name)
 
 
-def mk_temp_subdir(forbidden_names):
+def mk_temp_subdir(dir_path, forbidden_names):
     """
-    Make a temp subdir in pwd and return its name.
+    Make a temp subdir in pwd and return its path.
 
     The directory name is assigned randomly, and checked not to collide with
     `forbidden_names`.
     """
     for attempt in xrange(10):
-        fname = 'viren-' + hex(random.randint(1e9, 1e10))[2:]
-        if fname in forbidden_names or os.path.exists(fname):
+        name = 'viren-' + hex(random.randint(1e9, 1e10))[2:]
+        path = os.path.join(dir_path, name)
+        if name in forbidden_names or os.path.exists(path):
             continue
-        os.mkdir(fname)
-        return fname
-    raise VirenError("Failed to create temp dir")
+        os.mkdir(path)
+        return path
+    raise VirenError("Failed to create temp subdir")
+
+
+def get_names(dir_path):
+    """
+    Return list of filenames in given dir, in sorted order.
+    """
+    names = os.listdir(dir_path)
+    names.sort()
+    return names
+
+
+def do_rename(dir_path, new_names):
+    """
+    Rename files in dir_path (in sorted order) to new_names.
+
+    Leading or trailing whitespace in `new_names` is ignored.
+
+    Raises VirenError if the rename cannot be performed.
+    """
+    # Clean new_names and do some sanity checks.
+    new_names = [name.strip() for name in new_names]
+    old_names = get_names(dir_path)
+    verify_rename_list(old_names, new_names)
+
+    # Move the old files to a temp subdir. We do this to avoid accidental
+    # overwrites, in case old_names and new_names overlap.
+    subdir_path = mk_temp_subdir(dir_path, old_names + new_names)
+    for name in old_names:
+        old_path = os.path.join(dir_path, name)
+        new_path = os.path.join(subdir_path, name)
+        ret = subprocess.call(['mv', old_path, new_path])
+        if ret != 0:
+            raise VirenError('mv failed with return code {}'.format(ret))
+
+    # Now move the files back with their new names.
+    for i in xrange(len(old_names)):
+        old_path = os.path.join(subdir_path, old_names[i])
+        new_path = os.path.join(dir_path, new_names[i])
+        ret = subprocess.call(['mv', old_path, new_path])
+        if ret != 0:
+            raise VirenError('mv failed with return code {}'.format(ret))
+
+    # Clean up.
+    os.rmdir(subdir_path)
 
 
 if __name__ == "__main__":
     # Get sorted list of files in pwd.
-    old_names = os.listdir('.')
-    old_names.sort()
-    index_to_old_name = dict(enumerate(old_names))
+    old_names = get_names('.')
 
     # Write that list to a temp file.
-    _, temp_path = tempfile.mkstemp(prefix='viren')
+    _, temp_path = tempfile.mkstemp(prefix='viren-')
     temp = open(temp_path, 'w')
     temp.write('\n'.join(old_names))
     temp.close()
@@ -68,29 +111,11 @@ if __name__ == "__main__":
         if ret != 0:
             raise VirenError('editor failed with return code {}'.format(ret))
 
-        # Get the edited list back from the temp file, and sanity check it.
-        temp = open(temp_path)
-        new_names = [line.strip() for line in temp.xreadlines()]
-        temp.close()
-        verify_rename_list(old_names, new_names)
-
-        # Move the old files to a temp subdir. We do this to avoid accidental
-        # overwrites, in case old_names and new_names overlap.
-        temp_dirname = mk_temp_subdir(old_names + new_names)
-        for name in old_names:
-            ret = subprocess.call(['mv', name, temp_dirname + os.path.sep])
-            if ret != 0:
-                raise VirenError('mv failed with return code {}'.format(ret))
-
-        # Now move the files back with their new names.
-        for i in xrange(len(old_names)):
-            ret = subprocess.call(
-                ['mv', os.path.join(temp_dirname, old_names[i]), new_names[i]])
-            if ret != 0:
-                raise VirenError('mv failed with return code {}'.format(ret))
+        # Get the edited list back from the temp file, and do the rename.
+        new_names = open(temp_path).readlines()
+        do_rename('.', new_names)
 
         # Clean up.
-        os.rmdir(temp_dirname)
         os.remove(temp_path)
         print "Done."
 
